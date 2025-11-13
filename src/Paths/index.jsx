4a0adf8f37175c75231739be6434eab1c1b1745c
Edimilson Borges <edimilson-borges159@hotmail.com>
2025-11-12 22:53:47 -0300

    feat:criado o menu e feito alguns agustes no projeto

--- src/Paths/index.jsx
+++ src/Paths/index.jsx
@@ -4,6 +4,11 @@ import HomePage from "../pages/HomePage";
 import NotFound from "../pages/NotFound";
 import FileManager from "../pages/FileManagePage";
 import LoginPage from "../pages/LoginPage";
+import AutoDj from "../pages/AutoDjPage";
+import AccountConfigPage from "../pages/AccountConfigPage";
+import LiveTransmissionPage from "../pages/LiveTransmissionPage";
+import PlaylistsPage from "../pages/PlaylistsPage";
+import StationConfigPage from "../pages/StationConfigPage";
 
 const Paths = () => {
     return (
@@ -12,8 +17,23 @@ const Paths = () => {
                 <Route path="/" element={<SiteLayout />}>
                     <Route index element={<HomePage />} />
                 </Route>
+                <Route path="/auto-dj" element={<SiteLayout />}>
+                    <Route index element={<AutoDj />} />
+                </Route>
+                <Route path="/playlists" element={<SiteLayout />}>
+                    <Route index element={<PlaylistsPage />} />
+                </Route>
                 <Route path="/file-manager" element={<SiteLayout />}>
                     <Route index element={<FileManager />} />
+                </Route>
+                <Route path="/live-transmission" element={<SiteLayout />}>
+                    <Route index element={<LiveTransmissionPage />} />
+                </Route>
+                <Route path="/account-config" element={<SiteLayout />}>
+                    <Route index element={<AccountConfigPage />} />
+                </Route>
+                <Route path="/station-config" element={<SiteLayout />}>
+                    <Route index element={<StationConfigPage />} />
                 </Route>
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="*" element={<NotFound />} />
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,8 +1,62 @@
-const Menu = () => {
-    return ( 
-        <>
-        </>
-     );
+import { NavLink } from "react-router-dom";
+import { User, Play, List, Folders, RadioTower, Cog } from "lucide-react";
+
+export default function Menu() {
+     {/*sm:	640px	Celulares maiores
+ md:	768px	Tablets
+lg:	1024px	Laptops
+xl:	1280px	Monitores grandes
+2xl: */}
+    return (
+        
+        <div className="pl-5 pt-5 flex flex-col sm:w-[500px] md:w-[500px] lg:w-[500px] xl:w-[500px] 2xl:w-[500px] gap-1">
+            <NavLink to="/"className={({ isActive }) =>
+                       `flex gap-2 p-3 items-center font-semibold text-lg rounded-md ${isActive
+                        ? "bg-blue-500 text-white"
+                        : "text-slate-800 hover:bg-blue-500 hover:text-white"}`}><User /> Perfil
+            </NavLink>
+
+            <NavLink to="/auto-dj" className={({ isActive }) =>
+                    `flex gap-3 p-3 items-center font-semibold text-lg rounded-md ${isActive
+                        ? "bg-blue-500 text-white"
+                        : "text-slate-800 hover:bg-blue-500 hover:text-white"}`}>
+                        <Play /> Auto DJ
+            </NavLink>
+
+            <NavLink to="/playlists"className={({ isActive }) =>
+                    `flex gap-3 p-3 items-center font-semibold text-lg rounded-md ${isActive
+                        ? "bg-blue-500 text-white"
+                        : "text-slate-800 hover:bg-blue-500 hover:text-white"}`}>
+                        <List /> Playlists
+            </NavLink>
+
+            <NavLink to="/file-manager" className={({ isActive }) =>
+                    `flex gap-3 p-3 items-center font-semibold text-lg rounded-md ${isActive
+                        ? "bg-blue-500 text-white"
+                        : "text-slate-800 hover:bg-blue-500 hover:text-white"}`}>
+                        <Folders /> Gerenciador de Arquivos
+            </NavLink>
+
+            <NavLink to="/live-transmission" className={({ isActive }) =>
+                    `flex gap-3 p-3 items-center font-semibold text-lg rounded-md ${isActive
+                        ? "bg-blue-500 text-white"
+                        : "text-slate-800 hover:bg-blue-500 hover:text-white"}`}>
+                <RadioTower /> Transmissão ao vivo
+            </NavLink>
+
+            <NavLink to="/account-config"className={({ isActive }) =>
+                    `flex gap-3 p-3 items-center font-semibold text-lg rounded-md ${isActive
+                        ? "bg-blue-500 text-white"
+                        : "text-slate-800 hover:bg-blue-500 hover:text-white"}`}>
+                <Cog /> Configuração da conta
+            </NavLink>
+
+            <NavLink to="/station-config" className={({ isActive }) =>
+                    `flex gap-3 p-3 items-center font-semibold text-lg rounded-md ${isActive
+                        ? "bg-blue-500 text-white"
+                        : "text-slate-800 hover:bg-blue-500 hover:text-white"}`}>
+                <Cog /> Configurações da estação
+            </NavLink>
+        </div>
+    );
 }
- 
-export default Menu;
